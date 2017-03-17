7f917be30bd77f17b083fa14fe228806099eb989
Antoine Pitrou <pitrou.antoine@gmail.com>
2017-03-16 17:55:42 -0700

    setup react

---
+++ app.js
@@ -0,0 +1,1 @@
+console.log("Thanks God it works!")
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: {
+    'app': './app.js'
+  },
+  output: {
+    path: './dist',
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      query: {
+        presets: [
+          'react'
+        ],
+        plugins: []
+      }
+    }]
+  }
+}
